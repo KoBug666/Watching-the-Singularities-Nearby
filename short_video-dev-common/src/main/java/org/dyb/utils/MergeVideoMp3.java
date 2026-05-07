5963d5e27880215d3a9fb5046781544afc369c15
KoBug666 <1549691706@qq.com>
2026-05-07 21:26:53 +0800

    Add files via upload

---
+++ short_video-dev-common/src/main/java/org/dyb/utils/MergeVideoMp3.java
@@ -0,0 +1,109 @@
+package org.dyb.utils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MergeVideoMp3 {
+
+	private String ffmpegEXE;
+	
+	public MergeVideoMp3(String ffmpegEXE) {
+		super();
+		this.ffmpegEXE = ffmpegEXE;
+	}
+	
+	public void removeVoice(String videoInputPath,String videoOutputPath) throws Exception {
+//		ffmpeg.exe -i 1.mp4 -vcodec copy -an 11.mp4
+		List<String> command = new ArrayList<>();
+		command.add(ffmpegEXE);
+		
+		command.add("-i");
+		command.add(videoInputPath);
+		
+		command.add("-vcodec");		
+		command.add("copy");
+		
+		command.add("-an");
+		command.add(videoOutputPath);
+		
+		ProcessBuilder builder = new ProcessBuilder(command);
+		Process process = builder.start();
+		
+		InputStream errorStream = process.getErrorStream();
+		InputStreamReader inputStreamReader = new InputStreamReader(errorStream);
+		BufferedReader br = new BufferedReader(inputStreamReader);
+		
+		String line = "";
+		while ( (line = br.readLine()) != null ) {
+		}
+		
+		if (br != null) {
+			br.close();
+		}
+		if (inputStreamReader != null) {
+			inputStreamReader.close();
+		}
+		if (errorStream != null) {
+			errorStream.close();
+		}
+		
+	}
+	
+	public void convertor(String videoInputPath, String mp3InputPath,
+			double seconds, String videoOutputPath) throws Exception {
+//		ffmpeg.exe -i 11.mp4 -i 1.mp3 -t 18 -y new.mp4
+		List<String> command = new ArrayList<>();
+		command.add(ffmpegEXE);
+		
+		command.add("-i");
+		command.add(videoInputPath);
+		
+		command.add("-i");
+		command.add(mp3InputPath);
+		
+		command.add("-t");
+		command.add(String.valueOf(seconds));
+		
+		command.add("-y");
+		command.add(videoOutputPath);
+		
+		ProcessBuilder builder = new ProcessBuilder(command);
+		Process process = builder.start();
+		
+		InputStream errorStream = process.getErrorStream();
+		InputStreamReader inputStreamReader = new InputStreamReader(errorStream);
+		BufferedReader br = new BufferedReader(inputStreamReader);
+		
+		String line = "";
+		while ( (line = br.readLine()) != null ) {
+		}
+		
+		if (br != null) {
+			br.close();
+		}
+		if (inputStreamReader != null) {
+			inputStreamReader.close();
+		}
+		if (errorStream != null) {
+			errorStream.close();
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		MergeVideoMp3 ffmpeg = new MergeVideoMp3("C:\\ffmpeg\\bin\\ffmpeg.exe");
+		try {
+			ffmpeg.removeVoice("E:\\moon.mp4", "E:\\11.mp4");
+			ffmpeg.convertor("E:\\11.mp4", "E:\\理想三旬.mp3", 18, "E:\\new.mp4");
+			File file=new File("E:\\11.mp4");
+			file.delete();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
